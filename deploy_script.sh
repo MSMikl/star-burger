47ee7b39dfa552fa54bc98a547973f7150907747
Mikl <michalbl4@gmail.com>
2022-09-13 15:45:30 +0300

    deploy script edited

--- deploy_script.sh
+++ deploy_script.sh
@@ -2,6 +2,7 @@
 set -e
 cd ~/python/star-burger
 git pull
+version=`git rev-parse HEAD`
 source venv/bin/activate
 pip install -r requirements.txt
 npm ci --dev
@@ -9,4 +10,5 @@ npm ci --dev
 python3 manage.py migrate
 systemctl restart burger-server
 systemctl reload nginx
+ curl -H "X-Rollbar-Access-Token: 452fe41d343c43c78ed9bb6787c71afb" -H "Content-Type: application/json" -X POST 'https://api.rollbar.com/api/1/deploy' -d '{"environment": "production_server", "revision": "'${version}'", "rollbar_name": "mikl", "local_username": "mikl", "comment": "", "status": "succeeded"}'
 echo 'Deployment successful'
