05a7f0090464c7e3e144c73423dd6447975636b2
Mikl <michalbl4@gmail.com>
2022-09-21 20:15:35 +0300

    deploy script edited

--- deploy_script.sh
+++ deploy_script.sh
@@ -7,6 +7,8 @@ source venv/bin/activate
 pip install -r requirements.txt
 python3 manage.py collectstatic --noinput
 python3 manage.py migrate --noinput
+npm ci --dev
+./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
 systemctl restart burger-server
 systemctl reload nginx
  curl -H "X-Rollbar-Access-Token: 452fe41d343c43c78ed9bb6787c71afb" -H "Content-Type: application/json" -X POST 'https://api.rollbar.com/api/1/deploy' -d '{"environment": "production_server", "revision": "'${version}'", "rollbar_name": "mikl", "local_username": "mikl", "comment": "", "status": "succeeded"}'
